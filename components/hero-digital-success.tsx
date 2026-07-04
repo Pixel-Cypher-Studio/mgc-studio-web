5d361da009fca2058834fcf8f234039911ee368b
MandavkarPranjal <programmer.pranjal@gmail.com>
2026-07-04 22:19:09 +0530

    add: logo

--- components/hero-digital-success.tsx
+++ components/hero-digital-success.tsx
@@ -104,17 +104,11 @@ export const HeroDigitalSuccess = () => {
             >
               <nav className="space-y-4">
                 <div className="flex items-center gap-2 text-white">
-                  <svg
-                    className="h-8 w-8 fill-white"
-                    width="97"
-                    height="108"
-                    viewBox="0 0 97 108"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M55.5 0C61.0005 0.00109895 64.5005 2.50586 64.5 7.5V17C64.5 24.5059 68.5005 27.5 81 27.5H88C94.0005 27.5059 96.5 29.5059 96.5 37.5V98.5C96.5 106.006 95.0005 107.5 88 107.5H41.5C36.5005 107.5 32 104.506 32 98.5V88C32 84.5 28.5 80 20.5 80H8.5C3 80 0 76.5 0 71.5V6.5C0.00048844 1.50586 2.50049 0.00585937 8.5 0H55.5ZM31 20C28.7909 20 27 21.7909 27 24V74C27 76.2091 28.7909 78 31 78H58C60.2091 78 62 76.2091 62 74V24C62 21.7909 60.2091 20 58 20H31Z" />
-                  </svg>
-                  <span>UI-Layouts</span>
+                  <img
+                    src="/MGCSTUDIO.svg"
+                    alt="MCG Studio Logo"
+                    className="h-4 w-auto md:h-5"
+                  />
                 </div>
                 <a
                   href="#"
@@ -149,16 +143,11 @@ export const HeroDigitalSuccess = () => {
         {!isMobile && (
           <header className="relative flex items-center justify-between p-4 px-10">
             <div className="flex items-center gap-2">
-              <svg
-                className="h-8 w-8 fill-white"
-                width="97"
-                height="108"
-                viewBox="0 0 97 108"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M55.5 0C61.0005 0.00109895 64.5005 2.50586 64.5 7.5V17C64.5 24.5059 68.5005 27.5 81 27.5H88C94.0005 27.5059 96.5 29.5059 96.5 37.5V98.5C96.5 106.006 95.0005 107.5 88 107.5H41.5C36.5005 107.5 32 104.506 32 98.5V88C32 84.5 28.5 80 20.5 80H8.5C3 80 0 76.5 0 71.5V6.5C0.00048844 1.50586 2.50049 0.00585937 8.5 0H55.5ZM31 20C28.7909 20 27 21.7909 27 24V74C27 76.2091 28.7909 78 31 78H58C60.2091 78 62 76.2091 62 74V24C62 21.7909 60.2091 20 58 20H31Z" />
-              </svg>
+              <img
+                src="/MGCSTUDIO.svg"
+                alt="MCG Studio Logo"
+                className="h-5 w-auto md:h-6"
+              />
             </div>
 
             <nav className="hidden items-center gap-12 text-sm font-medium text-white md:flex">
